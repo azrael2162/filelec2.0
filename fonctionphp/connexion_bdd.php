8be8b5c196eaedb47564c2186efb8e2c2e08bdfd
azrael2162 <vincent.pinelli2162@gmail.com>
2018-05-03 15:26:10 +0200

    Update connexion_bdd.php

--- fonctionphp/connexion_bdd.php
+++ fonctionphp/connexion_bdd.php
@@ -3,7 +3,7 @@ try {
   $ip="localhost";
   $bdd="bdd";
   $user="root";
-  $password="toto";
+  $password="";
     $db = new PDO("mysql:host=$ip;dbname=$bdd","$user","$password");
 } catch (Exception $e) {
   echo "erreur avec mysql-server un mail à été envoyé à l'admin";
